9168ef05bde8ab7f9f592d952cd9443ab290bba8
Filo Connesso <63156681+filoconnesso@users.noreply.github.com>
2021-10-24 23:28:23 +0200

    Update MinecraftReceiver.ino

--- arduino_example/MinecraftReceiver/MinecraftReceiver.ino
+++ arduino_example/MinecraftReceiver/MinecraftReceiver.ino
@@ -1,17 +1,55 @@
-void setup() {
-  Serial.begin(115200);
-  pinMode(6, OUTPUT);
-  digitalWrite(6, LOW);
+//Minecraft Receiver Example
+
+#define LED 6
+
+class Arducraft {
+  private :
+    Stream* s;
+  public :
+    Arducraft() {}
+    void begin(Stream *newSerRef);
+    bool readWord(const char* w);
+    String readMessage();
+};
+
+void Arducraft::begin(Stream *newSerRef) {
+  s = newSerRef;
 }
 
-void loop() { 
-  if (Serial.available()) {
-    String message = Serial.readStringUntil('\n');
-    if(message.indexOf("ledon") != -1) {
-      digitalWrite(6, 1);
-    }
-    if(message.indexOf("ledoff") != -1) {
-      digitalWrite(6, 0);
-    }
+bool Arducraft::readWord(const char* w) {
+  bool result;
+  String msg = s->readStringUntil('\n');
+  if (msg.indexOf(w) != -1) {
+    result = true;
+  } else {
+    result = false;
   }
+  return result;
+}
+
+String Arducraft::readMessage() {
+  return s->readStringUntil('\n');
+}
+
+Arducraft minecraft;
+
+void setup() {
+  Serial.begin(115200);
+  minecraft.begin(&Serial);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+
+  bool cmdLedON = minecraft.readWord("ledon");
+  bool cmdLedOFF = minecraft.readWord("ledoff");
+
+  if (cmdLedON) {
+    digitalWrite(LED, HIGH);
+  }
+
+  if (cmdLedOFF) {
+    digitalWrite(LED, LOW);
+  }
+
 }
