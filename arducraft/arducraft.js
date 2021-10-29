e579b0e32fa7c3fb1ce411e421aa8c97413e8749
alessandromrc <66976091+alessandromrc@users.noreply.github.com>
2021-10-29 16:38:59 +0200

    pablo release

--- arducraft/arducraft.js
+++ arducraft/arducraft.js
@@ -1,17 +1,31 @@
 const mineflayer = require("mineflayer");
 const serialPort = require("serialport");
-const read = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const inquirer = require("inquirer");
 
-read.question("Indicate the port : \n", (new_com) => {
-  if (new_com != "") {
-    console.log("Arducraft Daemon is running...");
-    device = new serialPort(new_com, { baudRate: 115200 });
-    MainBot();
-  }
-});
+let data = {};
+
+const questions = [
+  {
+    type: "input",
+    name: "serial_port",
+    message: "Indicate the serial port: ",
+  },
+  {
+    type: "input",
+    name: "hostname",
+    message: "Indicate the hostname: ",
+  },
+  {
+    type: "input",
+    name: "port",
+    message: "Indicate the port: ",
+  },
+  {
+    type: "input",
+    name: "bot_name",
+    message: "Indicate the bot name: ",
+  },
+];
 
 console.log("#####################################");
 console.log("#                                   #");
@@ -37,35 +51,43 @@ serialPort.list().then(
   (err) => console.log(err)
 );
 
-function MainBot() {
-  const bot = mineflayer.createBot({
-    host: "fcadventures.mcpro.io", // minecraft server ip
-    port: 25565, // only set if you need a port that isn't 25565
-    // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-    // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
-    username: "FiloIndianoBot",
+function Main() {
+  inquirer
+    .prompt(questions)
+
+    .then((answers) => {
+      const bot = mineflayer.createBot({
+        host: answers.hostname,
+        port: parseInt(answers.port),
+        username: answers.bot_name,
+      });
+      console.log(answers.serial_port)
+      const device = new serialPort(answers.serial_port, { baudRate: 115200 });
+
+
+      bot.on("chat", (username, message) => {
+        if (username === bot.username) return;
+        console.log(`${username} said "${message}"`);
+        device.write(message + "\n");
+      });
+    
+      function lookAtPlayer() {
+        const playerFilter = (entity) => entity.type === "player";
+        const playerEntity = bot.nearestEntity(playerFilter);
+    
+        if (!playerEntity) return;
+    
+        const pos = playerEntity.position;
+        bot.lookAt(pos);
+      }
+    
+      bot.on("physicTick", lookAtPlayer);
+    
+      // Log errors and kick reasons:
+      bot.on("kicked", console.log);
+      bot.on("error", console.log);
+
   });
-
-  bot.on("chat", (username, message) => {
-    if (username === bot.username) return;
-    bot.chat("/time set day");
-    console.log(`${username} said "${message}"`);
-    device.write(message + "\n");
-  });
-
-  function lookAtPlayer() {
-    const playerFilter = (entity) => entity.type === "player";
-    const playerEntity = bot.nearestEntity(playerFilter);
-
-    if (!playerEntity) return;
-
-    const pos = playerEntity.position;
-    bot.lookAt(pos);
-  }
-
-  bot.on("physicTick", lookAtPlayer);
-
-  // Log errors and kick reasons:
-  bot.on("kicked", console.log);
-  bot.on("error", console.log);
 }
+
+Main();
