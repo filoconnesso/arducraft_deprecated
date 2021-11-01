80dbd2bfc921086f09614d44664c359ee5e06ff4
Filo Connesso <63156681+filoconnesso@users.noreply.github.com>
2021-11-01 02:38:58 +0100

    Update MCTest.ino

--- MCTest/MCTest.ino
+++ MCTest/MCTest.ino
@@ -1,43 +1,28 @@
 #include "Arducraft.h"
 
-#define LED 13
-#define LED2 8
-#define POT A0
-
 Minecraft mc;
+
 
 void setup() {
   Serial.begin(115200);
   mc.deamonAttach(&Serial);
-  pinMode(LED, OUTPUT);
-  pinMode(LED2, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  
+  mc.run();
 
   String cmd = mc.readMessage();
 
-  bool led2on = mc.ifContainsWord(cmd, "led2on");
-  bool led2off = mc.ifContainsWord(cmd, "led2off");
-
-  if(cmd.indexOf("ledon") >= 0) {
-    digitalWrite(LED, HIGH);
-  }
-  if(cmd.indexOf("ledoff") >= 0) {
-    digitalWrite(LED, LOW);
+  if(mc.ifContainsWord(cmd, "led4on")) {
+    digitalWrite(13, HIGH);
   }
 
-  if(led2on) {
-    digitalWrite(LED2, HIGH);
-  }
-  if(led2off) {
-    digitalWrite(LED2, LOW);
+  if(mc.ifContainsWord(cmd, "led4off")) {
+    digitalWrite(13, LOW);
   }
 
-  int value = analogRead(A0);
-
-  value = map(value, 0, 1023, 0, 23000);
-
-  mc.setTime(value);
+  mc.writeMessage(String(mc.getTime()));
 
 }
