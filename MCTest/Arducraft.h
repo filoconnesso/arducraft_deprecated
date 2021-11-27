80841b103fe92e3013230d82af78f050187d5ebc
Filo Connesso <63156681+filoconnesso@users.noreply.github.com>
2021-11-27 01:39:12 +0100

    Update Arducraft.h

--- MCTest/Arducraft.h
+++ MCTest/Arducraft.h
@@ -93,6 +93,101 @@
 #define ZOMBIE_VILLAGER "minacraft:zombie_villager"
 #define ZOMBIFIED_PIGLIN "minecraft:zombified_piglin"
 
+//Blocks
+#define AIR "minecraft:air"
+#define STONE "minecraft:stone"
+#define GRANITE "minecraft:granite"
+#define POLISHED_GRANITE "minecraft:polished_granite"
+#define DIORITE "minecraft:diorite"
+#define POLISHED_DIORITE "minecraft:polished_diorite"
+#define ANDESITE "minecraft:andesite"
+#define POLISHED_ANDESITE "minecraft:polished_andesite"
+#define GRASS_BLOCK "minecraft:grass_block"
+#define DIRT "minecraft:dirt"
+#define COARSE_DIRT "minecraft:coarse_dirt"
+#define PODZOL "minecraft:podzol"
+#define COBBLESTONE "minecraft:cobblestone"
+#define OAK_PLANKS "minecraft:oak_planks"
+#define SPURCE_PLANKS "minecraft:spurce_planks"
+#define BIRCH_PLANKS "minecraft:birch_planks"
+#define JUNGLE_PLANKS "minecraft:jungle_planks"
+#define ACACIA_PLANKS "minecraft:acacia_planks"
+#define DARK_OAK_PLANKS "minecraft:dark_oak_planks"
+#define BEDROCK "minecraft:bedrock"
+#define SAND "minecraft:sand"
+#define RED_SAND "minecraft:red_sand"
+#define OAK_LOG "minecraft:oak_log"
+#define SPURCE_LOG "minecraft:spurce_log"
+#define BIRCH_LOG "minecraft:birch_log"
+#define JUNGLE_LOG "minecraft:jungle_log"
+#define ACACIA_LOG "minecraft:acacia_log"
+#define DARK_OAK_LOG "minecraft:dark_oak_log"
+#define STRIPPED_OAK_LOG "minecraft:stripped_oak_log"
+#define STRIPPED_SPURCE_LOG "minecraft:stripped_spurce_log"
+#define STRIPPED_BIRCH_LOG "minecraft:stripped_birch_log"
+#define STRIPPED_JUNGLE_LOG "minacraft:stripped_jungle_log"
+#define STRIPPED_ACACIA_LOG "minecraft:stripped_acacia_log"
+#define STRIPPED_DARK_OAK_LOG "minacraft:stripped_dark_oak_log"
+#define STRIPPED_OAK_WOOD "minecraft:stripped_oak_wood"
+#define STRIPPED_SPURCE_WOOD "minacraft:stripped_spurce_wood"
+#define STRIPPED_BIRCH_WOOD "minecraft:stripped_birch_wood"
+#define STRIPPED_JUNGLE_WOOD "minecraft:stripped_jungle_wood"
+#define STRIPPED_ACACIA_WOOD "minecraft:stripped_acacia_wood"
+#define STRIPPED_DARK_OAK_WOOD "minecraft:stripped_oak_wood"
+#define OAK_WOOD "minecraft:oak_wood"
+#define SPURCE_WOOD "minecraft:spurce_wood"
+#define BIRCH_WOOD "minecraft:birch_wood"
+#define JUNGLE_WOOD "minecraft:jungle_wood"
+#define ACACIA_WOOD "minecraft:acacia_wood"
+#define DARK_OAK_WOOD "minecraft:dark_oak_wood"
+#define GLASS "minacraft:glass"
+#define OAK_SLAB "minecraft:oak_slab"
+#define SPURCE_SLAB "minacraft:spurce_slab"
+#define BIRCH_SLAB "minecraft:birch_slab"
+#define JUNGLE_SLAB "minecraft:jungle_slab"
+#define ACACIA_SLAB "minacraft:acacia_slab"
+#define DARK_OAK_SLAB "minecraft:dark_oak_slab"
+#define STONE_SLAB "minecraft:stone_slab"
+#define COBBLESTONE_SLAB "minacraft:cobblestone_slab"
+#define BRICK_SLAB "minecraft:brick_slab"
+#define BRICKS "minecraft:bricks"
+#define TNT "minacraft:tnt"
+#define BOOKSHELF "minecraft:bookshelf"
+#define OBSIDIAN "minecraft:obsidian"
+#define WHITE_WOOL "minecraft:white_wool"
+#define ORANGE_WOOL "minecraft:orange_wool"
+#define MAGENTA_WOOL "minecraft:magenta_wool"
+#define LIGHT_BLUE_WOOL "minecraft:light_blue_wool"
+#define YELLOW_WOOL "minecraft:yellow_wool"
+#define LIME_WOOL "minecraft:live_wool"
+#define PINK_WOOL "minecraft:pink_wool"
+#define GRAY_WOOL "minecraft:gray_wool"
+#define LIGHT_GRAY_WOOL "minacraft:light_gray_wool"
+#define CYAN_WOOL "minecraft:cyan_wool"
+#define PURPLE_WOOL "minecraft:purple_wool"
+#define BLUE_WOOL "minecraft:blue_wool"
+#define BROWN_WOOL "minacraft:brown_wool"
+#define GREEN_WOOL "minacraft:green_wool"
+#define RED_WOOL "minecraft:red_wool"
+#define BLACK_WOOL "minecraft:black_wool"
+#define WHITE_TERRACOTTA "minecraft:white_terracotta"
+#define ORANGE_TERRACOTTA "minecraft:orange_terracotta"
+#define MAGENTA_TERRACOTTA "minecraft:magenta_terracotta"
+#define LIGHT_BLUE_TERRACOTTA "minecraft:light_blue_terracotta"
+#define YELLOW_TERRACOTTA "minecraft:yellow_terracotta"
+#define LIME_TERRACOTTA "minecraft:lime_terracotta"
+#define PINK_TERRACOTTA "minecraft:pink_terracotta"
+#define GRAY_TERRACOTTA "minecraft:gray_terracotta"
+#define LIGHT_GRAY_TERRACOTTA "minecraft:light_gray_terracotta"
+#define CYAN_TERRACOTTA "minecraft:cyan_terracotta"
+#define PURPLE_TERRACOTTA "minecraft:purple_terracotta"
+#define BLUE_TERRACOTTA "minecraft:blue_terracotta"
+#define BROWN_TERRACOTTA "minecraft:brown_terracotta"
+#define GREEN_TERRACOTTA "minecraft:green_terracotta"
+#define RED_TERRACOTTA "minecraft:red_terracotta"
+#define BLACK_TERRACOTTA "minecraft:black_terracotta"
+#define TERRACOTTA "minecraft:terracotta"
+
 String splitString(String string, char separator, int index)
 {
   int found = 0;
@@ -108,18 +203,18 @@ String splitString(String string, char separator, int index)
   return found > index ? string.substring(strIndex[0], strIndex[1]) : "";
 }
 
-class Debouncer
+class MinecraftButton
 {
   private:
     uint8_t btn;
     uint16_t state;
   public:
-    void begin(uint8_t button) {
+    MinecraftButton(uint8_t button, uint8_t mode) {
       btn = button;
       state = 0;
-      pinMode(btn, INPUT_PULLUP);
+      pinMode(btn, mode);
     }
-    bool debounce() {
+    bool pressed() {
       state = (state << 1) | digitalRead(btn) | 0xfe00;
       return (state == 0xff00);
     }
@@ -176,6 +271,16 @@ class Minecraft {
     bool isRaining();
     bool botReady();
     int PingValue();
+    double getBotPositionX();
+    double getBotPositionY();
+    double getBotPositionZ();
+    void placeBlock(String block);
+    void botGoForward();
+    void botGoBack();
+    void botGoLeft();
+    void botGoRight();
+    void botStop();
+    void botJump();
 };
 
 void Minecraft::deamonAttach(Stream * newserial) {
@@ -302,6 +407,37 @@ void Minecraft::createEntity(String entity) {
   this -> serial -> print("\n");
 }
 
+void Minecraft::placeBlock(String block) {
+  this -> serial -> print("[ARDUINO CMD] bot placeblock ");
+  this -> serial -> print(block);
+  this -> serial -> print("\n");
+}
+
+void Minecraft::botGoForward() {
+  this -> serial -> print("[ARDUINO CMD] bot walk forward\n");
+}
+
+void Minecraft::botGoBack() {
+  this -> serial -> print("[ARDUINO CMD] bot walk back\n");
+}
+
+void Minecraft::botGoLeft() {
+  this -> serial -> print("[ARDUINO CMD] bot walk left\n");
+}
+
+void Minecraft::botGoRight() {
+  this -> serial -> print("[ARDUINO CMD] bot walk right\n");
+}
+
+void Minecraft::botStop() {
+  this -> serial -> print("[ARDUINO CMD] bot walk stop\n");
+}
+
+
+void Minecraft::botJump() {
+  this -> serial -> print("[ARDUINO CMD] bot jump\n");
+}
+
 bool Minecraft::botIsSpawned() {
   return spawn;
 }
@@ -362,6 +498,18 @@ int Minecraft::PingValue() {
   return pingValue;
 }
 
+double Minecraft::getBotPositionX() {
+  return botPositionX;
+}
+
+double Minecraft::getBotPositionY() {
+  return botPositionY;
+}
+
+double Minecraft::getBotPositionZ() {
+  return botPositionZ;
+}
+
 void Minecraft::run() {
   curMillis = millis();
   if ((unsigned long)curMillis - prevMillis >= timerMillis) {
@@ -377,8 +525,7 @@ void Minecraft::run() {
       String deathValue = splitString(lastMessage, ';', 3);
       String healthValue = splitString(lastMessage, ';', 4);
       String errorValue = splitString(lastMessage, ';', 5);
-      String pingVal = splitString(lastMessage, ';', 14);
-
+      
       if (spawnValue == "true") spawn = true;
       if (spawnValue == "false") spawn = false;
 
@@ -410,7 +557,12 @@ void Minecraft::run() {
       if (rainingValue == "true") raining = true;
       if (rainingValue == "false") raining = false;
 
-      pingValue = pingVal.toInt();
+      pingValue = splitString(lastMessage, ';', 14).toInt();
+
+      botPositionX = splitString(lastMessage, ';', 15).toFloat();
+      botPositionY = splitString(lastMessage, ';', 16).toFloat();
+      botPositionZ = splitString(lastMessage, ';', 17).toFloat();
+
     }
 
     this->serial->flush();
