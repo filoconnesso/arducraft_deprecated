7c22f82c3532bf4910722013ef900bffae1276b0
Filo Connesso <63156681+filoconnesso@users.noreply.github.com>
2021-11-27 01:38:13 +0100

    Update arducraft.js

--- arducraft/arducraft.js
+++ arducraft/arducraft.js
@@ -3,6 +3,7 @@ const serialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 const inquirer = require("inquirer");
 const { restoreDefaultPrompts } = require("inquirer");
+const Vec3 = require("vec3").Vec3;
 
 let data = {};
 let isArduinoReady = false;
@@ -52,7 +53,7 @@ serialPort.list().then((ports) => {
       name: "port",
       message: "Indicate the port: ",
       default() {
-        return "61989";
+        return "63091";
       },
     },
     {
@@ -68,7 +69,7 @@ serialPort.list().then((ports) => {
       name: "bot_name",
       message: "Indicate the bot name: ",
       default() {
-        return "teensynator6000";
+        return "ArduCraft";
       },
     },
   ];
@@ -88,27 +89,88 @@ async function Main() {
         version: answers.version,
       });
 
-      function lookAtPlayer() {
-        const playerFilter = (entity) => entity.type === "player";
-        const playerEntity = bot.nearestEntity(playerFilter);
-        if (!playerEntity) return;
-        const pos = playerEntity.position;
-        bot.lookAt(pos);
-      }
-
       const device = new serialPort(answers.serial_port, {
         baudRate: 115200,
       });
 
       const parser = device.pipe(new Readline({ delimiter: "\n" }));
 
+      let moving = false;
+      let MinecraftGameInformation;
+
+      function botStop() {
+        if (botStarted) {
+          bot.setControlState("forward", false);
+          bot.setControlState("back", false);
+          bot.setControlState("left", false);
+          bot.setControlState("right", false);
+          bot.setControlState("jump", false);
+          bot.lookAt(bot.entity.position.offset(0, bot.entity.height, 0));
+          moving = false;
+        }
+      }
+
+      let maxRefZ;
+      let maxRefX;
+      let minRefZ;
+      let minRefX;
+      let maxRefY;
+      let minRefY;
+
       const ReadSerialPort = async () => {
         parser.on("data", (data) => {
           if (botStarted) {
             let command = data;
             command = command.replace(/(\r\n|\n|\r)/gm, "");
-            console.log(`BOT CONSOLE >>> ${command}`);
-            bot.chat(command);
+            if (command.indexOf("[ARDUINO CMD] bot walk forward") > -1) {
+              maxRefZ = Math.floor(bot.entity.position.z) + 1.0;
+              maxRefX = Math.floor(bot.entity.position.x) + 1.0;
+              minRefZ = Math.floor(bot.entity.position.z);
+              minRefX = Math.floor(bot.entity.position.x);
+              bot.setControlState("forward", true);
+              moving = true;
+            } else if (command.indexOf("[ARDUINO CMD] bot walk back") > -1) {
+              maxRefZ = Math.floor(bot.entity.position.z) + 1.0;
+              maxRefX = Math.floor(bot.entity.position.x) + 1.0;
+              minRefZ = Math.floor(bot.entity.position.z);
+              minRefX = Math.floor(bot.entity.position.x);
+              bot.setControlState("back", true);
+              moving = true;
+            } else if (command.indexOf("[ARDUINO CMD] bot walk left") > -1) {
+              maxRefZ = Math.floor(bot.entity.position.z) + 1.0;
+              maxRefX = Math.floor(bot.entity.position.x) + 1.0;
+              minRefZ = Math.floor(bot.entity.position.z);
+              minRefX = Math.floor(bot.entity.position.x);
+              bot.setControlState("left", true);
+              moving = true;
+            }
+            if (command.indexOf("[ARDUINO CMD] bot walk right") > -1) {
+              maxRefZ = Math.floor(bot.entity.position.z) + 1.0;
+              maxRefX = Math.floor(bot.entity.position.x) + 1.0;
+              minRefZ = Math.floor(bot.entity.position.z);
+              minRefX = Math.floor(bot.entity.position.x);
+              bot.setControlState("right", true);
+              moving = true;
+            }
+            if (command.indexOf("[ARDUINO CMD] bot jump") > -1) {
+              maxRefY = Math.floor(bot.entity.position.y) + 1.0;
+              minRefY = Math.floor(bot.entity.position.y);
+              bot.setControlState("jump", true);
+              moving = true;
+            }
+            if (command.indexOf("[ARDUINO CMD] bot walk stop") > -1) {
+              botStop();
+            } else if (command.indexOf("[ARDUINO CMD] bot placeblock") > -1) {
+              let currentBlock = command.replace("[ARDUINO CMD] bot placeblock","");
+              currentBlock = currentBlock.trim(currentBlock);
+              bot.chat("/fill ~ ~ ~ ~ ~ ~ " + currentBlock);
+            } else {
+              if (!moving) {
+                command = command.trim(command);
+                console.log(`BOT CONSOLE >>> ${command}`);
+                bot.chat(command);
+              }
+            }
           }
         });
       };
@@ -135,6 +197,9 @@ async function Main() {
           botGameMode: null,
           raining: false,
           ping: 0,
+          x: 0,
+          y: 0,
+          z: 0,
         },
       ];
 
@@ -155,10 +220,35 @@ async function Main() {
         });
       }
 
+      bot.on("move", function () {
+        if (moving) {
+          if (
+            bot.entity.position.x >= maxRefX ||
+            bot.entity.position.x <= minRefX
+          ) {
+            botStop();
+            return;
+          }
+          if (
+            bot.entity.position.z >= maxRefZ ||
+            bot.entity.position.z <= minRefZ
+          ) {
+            botStop();
+            return;
+          }
+          if (bot.entity.position.y > maxRefY) {
+            botStop();
+            return;
+          }
+        }
+      });
+
       bot.on("spawn", function () {
         console.log("BOT CONSOLE >>> The bot appeared on the map");
         minecraft_datas[0]["spawn"] = true;
         botStarted = true;
+        MinecraftGameInformation = require("minecraft-data")(bot.version);
+        bot.lookAt(bot.entity.position.offset(0, bot.entity.height, 0));
       });
       bot.on("kicked", function () {
         console.log("BOT CONSOLE >>> The bot got kicked");
@@ -184,14 +274,18 @@ async function Main() {
 
       function sendMinecraftData() {
         if (botStarted) {
+          let axis = bot.entity.position;
           minecraft_datas[0]["worldTime"] = bot.time.timeOfDay;
           minecraft_datas[0]["botExperience"] = bot.experience.points;
           minecraft_datas[0]["botHealth"] = bot.health;
           minecraft_datas[0]["botFood"] = bot.food;
           minecraft_datas[0]["botOxygen"] = bot.oxygenLevel;
           minecraft_datas[0]["botGameMode"] = bot.game.gameMode;
-          minecraft_datas[0]["raining"] = !bot.isRaining;
+          minecraft_datas[0]["raining"] = bot.isRaining;
           minecraft_datas[0]["ping"] = bot.player.ping;
+          minecraft_datas[0]["x"] = axis.x;
+          minecraft_datas[0]["y"] = axis.y;
+          minecraft_datas[0]["z"] = axis.x;
           let minecraft_datas_string = "[DEAMON-CMD] datas ";
           minecraft_datas.forEach((datas) => {
             Object.entries(datas).forEach(([dataType, dataValue]) => {
@@ -200,7 +294,7 @@ async function Main() {
           });
           minecraft_datas_string += "endata";
           device.write(minecraft_datas_string + "\n");
-          if(minecraft_datas[0]["kicked"] || minecraft_datas[0]["error"]) {
+          if (minecraft_datas[0]["kicked"] || minecraft_datas[0]["error"]) {
             botStarted = false;
           }
           restoreBotDataLifeStatus();
@@ -209,13 +303,13 @@ async function Main() {
       setInterval(sendMinecraftData, 10);
 
       bot.on("chat", (username, message) => {
-        if(botStarted) {
+        if (botStarted) {
           if (username === bot.username) return;
-          console.log(`BOT CONSOLE >>> ${username} : ${message}`);
+          if (!moving) {
+            console.log(`BOT CONSOLE >>> ${username} : ${message}`);
+          }
           device.write(message + "\n");
         }
       });
-
-      bot.on("physicTick", lookAtPlayer);
     });
 }
