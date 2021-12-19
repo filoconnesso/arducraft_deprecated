21d6b83019d73716aa9384c98e699155aa02a949
Filo Connesso <63156681+filoconnesso@users.noreply.github.com>
2021-12-19 20:50:07 +0100

    Update Arducraft.h

--- MCTest/Arducraft.h
+++ MCTest/Arducraft.h
@@ -8,30 +8,125 @@
 #include "definitions/aliases.h"
 #include "definitions/entities.h"
 
+typedef void (*task_callback)();
+unsigned long sequencers_counter = 0UL;
+bool task_exists = false;
 
 class MinecraftButton
 {
   private:
     uint8_t btn;
-    uint16_t state;
+    bool state;
+    bool pressedValue = false;
   public:
     MinecraftButton(uint8_t button, uint8_t mode) {
       btn = button;
-      state = 0;
       pinMode(btn, mode);
+      state = digitalRead(btn);
     }
-    bool pressed() {
-      state = (state << 1) | digitalRead(btn) | 0xfe00;
-      return (state == 0xff00);
-    }
+    void pressed(void (*callback)());
 };
 
+void MinecraftButton::pressed(void (*callback)()) {
+  if (digitalRead(btn) != state) {
+    if (!pressedValue) {
+      callback();
+      pressedValue = true;
+    }
+  } else {
+    pressedValue = false;
+  }
+}
+
+struct tasks {
+  uint16_t       index;
+  uint16_t       tasks_counter;
+  task_callback  callback;
+  tasks *        next_task = NULL;
+};
+
+tasks *first_task =   NULL, *last_task = NULL;
+
+class MinecraftSequencer {
+  private :
+    unsigned long curMillis = millis();
+    unsigned long prevMillis = millis();
+    unsigned long curDelay;
+    unsigned long sequencer_id = sequencers_counter++;
+    unsigned long tasks_function_position_counter = 0;
+    unsigned long tasks_counter = 0;
+    bool sequencer_enabled = false;
+    unsigned long tasks_times = 0;
+    unsigned long cur_time_step = 0;
+    task_callback endCallback;
+  public :
+    MinecraftSequencer() {
+      prevMillis = millis();
+    }
+    void addTask(task_callback task);
+    void run();
+    void go(unsigned long newTimes, unsigned long newDelay, task_callback newEndCallback);
+};
+
+void MinecraftSequencer::addTask(task_callback task) {
+  tasks *new_task = new tasks;
+  new_task->index = this->sequencer_id;
+  new_task->callback = task;
+  new_task->tasks_counter = this->tasks_counter++;
+  if (first_task == NULL) {
+    first_task = new_task;
+  } else {
+    last_task->next_task = new_task;
+  }
+  last_task = new_task;
+  if (!task_exists) {
+    task_exists = true;
+  }
+}
+
+void MinecraftSequencer::run() {
+  if (task_exists) {
+    curMillis = millis();
+    if ((unsigned long) curMillis - prevMillis > curDelay) {
+      if (sequencer_enabled) {
+        for (tasks *this_task = first_task; this_task != NULL; this_task = this_task->next_task) {
+          if (this->sequencer_id == this_task->index) {
+            if (this_task->tasks_counter == this->tasks_function_position_counter) {
+              this_task->callback();
+            }
+          }
+        }
+        this->tasks_function_position_counter++;
+        this->cur_time_step++;
+        if (this->tasks_function_position_counter >= this->tasks_counter) {
+          this->tasks_function_position_counter = 0;
+        }
+        if (this->cur_time_step == this->tasks_times) {
+          this->tasks_function_position_counter = 0;
+          this->sequencer_enabled = false;
+          this->endCallback();
+        }
+      }
+      prevMillis = curMillis;
+    }
+  }
+}
+
+void MinecraftSequencer::go(unsigned long newTimes, unsigned long newDelay, task_callback newEndCallback) {
+  if (task_exists) {
+    this->sequencer_enabled = true;
+    this->cur_time_step = 0;
+    this->tasks_times = newTimes * 2;
+    this->curDelay = newDelay;
+    this->endCallback = newEndCallback;
+  }
+}
 
 class Minecraft {
   private:
     unsigned long curMillis = millis();
     unsigned long prevMillis = millis();
-    unsigned long timerMillis = 5;
+    unsigned long timerMillis = 70;
     String lastMessage = "";
     bool HandShakeBoot = false;
     bool spawn, kicked, end, death, health, error = false;
@@ -49,13 +144,13 @@ class Minecraft {
       int maxIndex = string.length() - 1;
       for (int i = 0; i <= maxIndex && found <= index; i++) {
         if (string.charAt(i) == separator || i == maxIndex) {
-        found++;
-        strIndex[0] = strIndex[1] + 1;
-        strIndex[1] = (i == maxIndex) ? i + 1 : i;
+          found++;
+          strIndex[0] = strIndex[1] + 1;
+          strIndex[1] = (i == maxIndex) ? i + 1 : i;
+        }
       }
+      return found > index ? string.substring(strIndex[0], strIndex[1]) : "";
     }
-    return found > index ? string.substring(strIndex[0], strIndex[1]) : "";
-  }
   public:
     Stream * serial;
     Minecraft() {
@@ -102,6 +197,8 @@ class Minecraft {
     void botGoRight();
     void botStop();
     void botJump();
+    void waitBot();
+    void lever(long x, long y, long z, bool powered);
 };
 
 void Minecraft::deamonAttach(Stream * newserial) {
@@ -122,10 +219,33 @@ bool Minecraft::ifContainsWord(String message, String word) {
   return response;
 }
 
+void Minecraft::waitBot() {
+  String curMessage;
+  while (curMessage.indexOf("[DEAMON-CMD] next") != 0) {
+    curMessage =  this->serial->readStringUntil('\n');
+  }
+}
+
 void Minecraft::writeMessage(String message) {
   this -> serial -> print("/say ");
   this -> serial -> print(message);
   this -> serial -> print("\n");
+}
+
+void Minecraft::lever(long x, long y, long z, bool powered) {
+  this -> serial -> print("/setblock ");
+  this -> serial -> print(x);
+  this -> serial -> print(" ");
+  this -> serial -> print(y);
+  this -> serial -> print(" ");
+  this -> serial -> print(z);
+  this -> serial -> print(" minecraft:lever[powered=");
+  if (powered) {
+    this -> serial -> print("true");
+  } else {
+    this -> serial -> print("false");
+  }
+  this -> serial -> print("] \n");
 }
 
 void Minecraft::setWorldTime(int value) {
@@ -235,28 +355,28 @@ void Minecraft::placeBlock(String block) {
 }
 
 void Minecraft::botGoForward() {
-  this -> serial -> print(Arduino_Cmd + "bot walk forward\n");
+  this -> serial -> print(Arduino_Cmd + "bot walk forward \n");
 }
 
 void Minecraft::botGoBack() {
-  this -> serial -> print(Arduino_Cmd + "bot walk back\n");
+  this -> serial -> print(Arduino_Cmd + "bot walk back \n");
 }
 
 void Minecraft::botGoLeft() {
-  this -> serial -> print(Arduino_Cmd + "bot walk left\n");
+  this -> serial -> print(Arduino_Cmd + "bot walk left \n");
 }
 
 void Minecraft::botGoRight() {
-  this -> serial -> print(Arduino_Cmd + "bot walk right\n");
+  this -> serial -> print(Arduino_Cmd + "bot walk right \n");
 }
 
 void Minecraft::botStop() {
-  this -> serial -> print(Arduino_Cmd + "bot walk stop\n");
+  this -> serial -> print(Arduino_Cmd + "bot walk stop \n");
 }
 
 
 void Minecraft::botJump() {
-  this -> serial -> print(Arduino_Cmd + "bot jump\n");
+  this -> serial -> print(Arduino_Cmd + "bot jump \n");
 }
 
 bool Minecraft::botIsSpawned() {
@@ -332,12 +452,15 @@ double Minecraft::getBotPositionZ() {
 }
 
 void Minecraft::run() {
+
   curMillis = millis();
+
   if ((unsigned long)curMillis - prevMillis >= timerMillis) {
 
     lastMessage =  this -> serial -> readStringUntil('\n');
 
-    if (ifContainsWord(lastMessage, "[DEAMON-CMD] datas ")) {
+    if (lastMessage.startsWith("[DEAMON-CMD] datas")) {
+
       lastMessage.replace("[DEAMON-CMD] datas ", "");
 
       String spawnValue = splitString(lastMessage, ';', 0);
@@ -347,12 +470,18 @@ void Minecraft::run() {
       String healthValue = splitString(lastMessage, ';', 4);
       String errorValue = splitString(lastMessage, ';', 5);
 
-      spawnValue == "true" ? spawn = true : spawn = false;
-      kickedValue == "true" ? kicked = true : kicked = false;
-      endValue == "true" ? end = true : end = false;
-      healthValue == "true" ? health = true : health = false;
-      deathValue == "true" ? death = true : death = false;
-      errorValue == "true" ? error = true : error = false;
+      if (spawnValue == "true") spawn = true;
+      if (spawnValue == "false") spawn = false;
+      if (kickedValue == "true") kicked = true;
+      if (kickedValue == "false") kicked = false;
+      if (endValue == "true") end = true;
+      if (endValue == "false") end = false;
+      if (healthValue == "true") health = true;
+      if (healthValue == "false") health = false;
+      if (deathValue == "true") death = true;
+      if (deathValue == "false") death = false;
+      if (errorValue == "true") error = true;
+      if (errorValue == "false") error = false;
 
       errorMessage = splitString(lastMessage, ';', 6);
       worldTime = splitString(lastMessage, ';', 7).toInt();
@@ -364,7 +493,8 @@ void Minecraft::run() {
 
       String rainingValue = splitString(lastMessage, ';', 13);
 
-      rainingValue == "true" ? raining = true : raining = false;
+      if (rainingValue == "true") raining = true;
+      if (rainingValue == "false") raining = false;
 
       pingValue = splitString(lastMessage, ';', 14).toInt();
 
@@ -378,6 +508,7 @@ void Minecraft::run() {
     prevMillis = curMillis;
 
   }
+
 }
 
 #endif
